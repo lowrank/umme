7d1e91dd6eb86dfbe02eb233cbcb8fa60210125b
Yimin Zhong <yimin.zhong@duke.edu>
2021-08-14 22:51:04 -0400

    update data

--- Ngsolve/visualize.m
+++ Ngsolve/visualize.m
@@ -1,8 +1,9 @@
-function visualize(v, f, data)
-    figure(1);
+function visualize(v, f, data, str)
+    h = figure(1);
     set(gcf,'Position',[100 100 500 400]);
     trisurf(f', v(1,:), v(2,:), data, 'EdgeColor', 'None');
     shading interp;colorbar;colormap jet;
     view(2);
+    saveas(h, str);
 end
 
